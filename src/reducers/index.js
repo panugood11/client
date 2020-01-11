2a6c434db613799f1acdcf51d1a7beb536e6fbfb
panugood11 <panugood@gmail.com>
2020-01-11 17:46:35 +0700

    73. การตั้งค่า Form reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
+import {reducer as reduxFrom} from "redux-form";
 import ProductReducer from "./ProductReducer";
 import OrderReducer from "./OrderReducer";
 
 const rootReducer = combineReducers({
     orders: OrderReducer,
-    products: ProductReducer
+    products: ProductReducer,
+    from : reduxFrom
 })
 
 export default rootReducer;

de90b1473ed80cc2cc3e18ea73b5d9e77796754f
panugood <panugood@gmail.com>
2020-01-18 17:28:43 +0700

    76. การใช้ Field Component #1

--- src/components/ProductItem/ProductFrom.js
+++ src/components/ProductItem/ProductFrom.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
-import {reduxForm} from "redux-form";
+import {reduxForm, Field} from "redux-form";
 
 class ProductFrom extends Component {
+
+    renderFields() {
+        const formFields = [
+            { name : "productName", type: "text"},
+            { name : "unitPrice", type: "number"},
+            { name : "thumbnail", type: "text"}
+        ];
+        return formFields.map(({name, type}) => {
+            return(
+                <Field name={name} type={type} component = "input" />
+            )
+        })
+    }
     
     render() {
         return(
             <div>
                 <from>
-
+                    {this.renderFields()}
                 </from>
             </div>
         )
